d4fabc31a36638d5dcf1d6f633f48fa1057482fe
Vesperi <1023625028@qq.com>
2026-09-06 17:05:34 +0800

    Initial commit: 三国杀（LBThreeCountry）项目初始化

---
+++ src/main/java/com/exercise/lbthreecountry/LbThreeCountryApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.lbthreecountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbThreeCountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbThreeCountryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/lbthreecountry/LbThreeCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.lbthreecountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbThreeCountryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
