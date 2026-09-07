a6b8c07de34214918885511b670c9d769a65536c
Vesperi <1023625028@qq.com>
2026-09-07 15:21:16 +0800

    feat: 添加用户实体类（待完善）

---
+++ src/main/java/com/lbthreecountry/entity/user/User.java
@@ -0,0 +1,66 @@
+package com.lbthreecountry.entity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 用户实体类
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    /**
+     * 用户ID
+     */
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * 头像
+     */
+    private String avatar;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 状态（0=禁用, 1=正常, 2=已删除, 3=管理员）
+     */
+    private Integer status;
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+    /**
+     * 最后登录时间
+     */
+    private LocalDateTime lastLoginTime;
+    /**
+     * 总游戏次数
+     */
+    private Integer totalGames;
+    /**
+     * 胜利次数
+     */
+    private Integer wins;
+}
