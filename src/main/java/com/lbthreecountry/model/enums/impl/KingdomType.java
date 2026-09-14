f2b3fcf89c12829feae616888261828f1da6f6bf
Vesperi <1023625028@qq.com>
2026-09-14 13:16:27 +0800

    fix: 修改势力枚举

--- src/main/java/com/lbthreecountry/model/enums/impl/KingdomType.java
+++ src/main/java/com/lbthreecountry/model/enums/impl/KingdomType.java
@@ -9,18 +9,22 @@ import com.lbthreecountry.model.enums.utils.EnumUtils;
  * 势力类型枚举
  */
 public enum KingdomType implements BaseEnum {
-    EUD(1,"艾欧尼亚"),
-    NOK(2,"诺克萨斯"),
-    FRE(3,"弗雷尔卓德"),
-    RUN(4,"符文大陆"),
-    PET(5,"皮尔特沃夫"),
-    DRA(6,"暗影岛"),
-    DEL(7,"班德尔城"),
-    GEM(8,"巨神锋"),
-    DMA(9,"德玛西亚"),
-    JOR(10,"比尔吉沃特"),
-    ETU(11,"以绪塔尔"),
-    AN(12,"祖安");
+    SHU(1,"蜀"),
+    WEI(2,"魏"),
+    WU(3,"吴"),
+    QUN(4,"群"),
+    EUD(101,"艾欧尼亚"),
+    NOK(102,"诺克萨斯"),
+    FRE(103,"弗雷尔卓德"),
+    RUN(104,"符文大陆"),
+    PET(105,"皮尔特沃夫"),
+    DRA(106,"暗影岛"),
+    DEL(107,"班德尔城"),
+    GEM(108,"巨神锋"),
+    DMA(109,"德玛西亚"),
+    JOR(110,"比尔吉沃特"),
+    ETU(111,"以绪塔尔"),
+    AN(112,"祖安");
 
     private final Integer code;
     private final String description;
